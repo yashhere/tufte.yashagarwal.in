085000ed3950182d2797001e3e1325310c7594bc
werckerbot <pleasemailus@wercker.com>
2018-03-02 06:20:42 +0000

    Deploy from yash2696

--- js/redirection.js
+++ js/redirection.js
@@ -1,4 +1,25 @@
-var host = "yashagarwal.in";
-if ((host == window.location.host) && (window.location.protocol != 'https:')) {
-  window.location = window.location.toString().replace(/^http:/, "https:");
+// var host = "yashagarwal.in";
+// if ((host == window.location.host) && (window.location.protocol != 'https:')) {
+//   window.location = window.location.toString().replace(/^http:/, "https:");
+// }
+
+//check if http && NOT localhost or 127.0.0.1
+function _httpsAlways() {
+    if (location.protocol == 'http:' && (location.hostname != "127.0.0.1" || location.hostname == "localhost"))
+        location.href = location.href.replace("http", "https");
 }
+
+//auto redirect from current *.gitlab.io to custom domain
+//send destination domain as domainTo (FQDN)
+function _notGitLab(domainTo = true) {
+    var x = location.hostname.search("gitlab.io") != -1 ? true : false; // true if *.gitlab.io
+    if (x) {
+        location.href = domainTo;
+    }
+}
+
+//invoke the functions
+window.onload = function() {
+    _notGitLab("https://yashagarwal.in");
+    _httpsAlways(); //no need as SSL address already passed in above request // NEED as notGitLab() works only if starting domain is *.gitlab.io
+};
