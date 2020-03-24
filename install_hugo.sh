065e31ea0efe1950a7ff2d64c3261c1938d28f70
Yash Agarwal <yashagarwaljpr@gmail.com>
2020-03-24 17:06:40 +0530

    adding gulp config

---
+++ install_hugo.sh
@@ -0,0 +1,2 @@
+VERSION=`curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
+curl -L https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz | tar -xz && mv hugo /usr/local/bin/hugo
