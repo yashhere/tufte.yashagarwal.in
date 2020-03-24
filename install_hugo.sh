5067da53f1e60f933a85b4a9bbdc1030d1d280d0
Yash Agarwal <yashagarwaljpr@gmail.com>
2020-03-24 13:09:09 +0000

    fix version parsing

--- install_hugo.sh
+++ install_hugo.sh
@@ -1,2 +1,3 @@
 VERSION=`curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
-curl -L https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz | tar -xz && mv hugo /usr/local/bin/hugo
+VERSION="${VERSION//v}"
+curl -L -s https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz | tar -xz && mv hugo /usr/local/bin/hugo
