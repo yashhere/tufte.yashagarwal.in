8e7470294bee48fc237350c9ade34d8f6ed61b45
Yash Agarwal <13733968+yashhere@users.noreply.github.com>
2020-03-25 18:47:34 +0530

    clean slate

--- install_hugo.sh
+++
@@ -1,3 +0,0 @@
-VERSION=`curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")'`
-VERSION="${VERSION//v}"
-curl -L -s https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz | tar -xz && mv hugo /usr/local/bin/hugo
